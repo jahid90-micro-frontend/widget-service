74b43578c2d94bbc2b975f601dd905f5ad9db14c
Jahid <jahid90@users.noreply.github.com>
2021-05-21 14:42:28 +0530

    log requests; move metadata to separate file

---
+++ src/data.js
@@ -0,0 +1,52 @@
+module.exports = {
+    metadata: {
+        'nav': {
+            id: 'nav',
+            content: {
+                ssi: {
+                    link: '/w/nav/fragment/nav'
+                },
+                csi: {
+                    tag: 'mf-nav',
+                    link: '/w/nav/common/Nav.js'
+                }
+            }
+        },
+        'footer': {
+            id: 'footer',
+            content: {
+                ssi: {
+                    link: '/w/footer/fragment/footer'
+                },
+                csi: {
+                    tag: 'mf-footer',
+                    link: '/w/footer/common/Footer.js'
+                }
+            }
+        },
+        'home': {
+            id: 'home',
+            content: {
+                ssi: {
+                    link: '/w/home/fragment/home'
+                },
+                csi: {
+                    tag: 'mf-home',
+                    link: '/w/home/common/Home.js'
+                }
+            }
+        },
+        'about': {
+            id: 'about',
+            content: {
+                ssi: {
+                    link: '/w/about/fragment/about'
+                },
+                csi: {
+                    tag: 'mf-about',
+                    link: '/w/about/common/About.js'
+                }
+            }
+        }
+    }
+};
--- src/server.js
+++ src/server.js
@@ -1,51 +1,17 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan');
+
+const { metadata } = require('./data.js');
 
 // Create the server
 const app = express();
 
 // Configuration
+app.use(morgan('tiny'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const metadata = {
-    'nav': {
-        id: 'nav',
-        content: {
-            ssi: {
-                link: '/nav/fragment/nav'
-            },
-            csi: {
-                tag: 'mf-nav',
-                link: '/nav/common/Nav.js'
-            }
-        }
-    },
-    'home': {
-        id: 'home',
-        content: {
-            ssi: {
-                link: '/home/fragment/home'
-            },
-            csi: {
-                tag: 'mf-home',
-                link: '/home/common/Home.js'
-            }
-        }
-    },
-    'footer': {
-        id: 'footer',
-        content: {
-            ssi: {
-                link: '/footer/fragment/footer'
-            },
-            csi: {
-                tag: 'mf-footer',
-                link: '/footer/common/Footer.js'
-            }
-        }
-    }
-};
 
 app.post('/', (req, res) => {
 
