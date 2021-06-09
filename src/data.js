8a47a54365dab166fd7b2545150a690e9166ee28
Jahid <jahid90@users.noreply.github.com>
2021-06-09 18:36:40 +0530

    add path prefix to widget content

--- src/data.js
+++ src/data.js
@@ -26,11 +26,11 @@ module.exports = {
         },
         'todos': {
             id: 'todos',
-            content: 'w/todos/'
+            content: '/w/todos/'
         },
         'minance': {
             id: 'minance',
-            content: 'w/minance/'
+            content: '/w/minance/'
         }
     }
 };
