5c249bf4496f0e5659c7d3207ea3cf813da6dad7
Jahid <jahid90@users.noreply.github.com>
2021-08-03 11:36:24 +0530

    fix widget uri key

--- src/data.js
+++ src/data.js
@@ -2,35 +2,35 @@ module.exports = {
     metadata: {
         'nav': {
             id: 'nav',
-            content: 'widgets/nav.html'
+            uri: 'common-widgets/nav.html'
         },
         'footer': {
             id: 'footer',
-            content: 'widgets/footer.html'
+            uri: 'common-widgets/footer.html'
         },
         'not-found': {
             id: 'not-found',
-            content: 'widgets/not-found.html'
+            uri: 'common-widgets/not-found.html'
         },
         'home': {
             id: 'home',
-            content: 'widgets/home.html'
+            uri: 'common-widgets/home.html'
         },
         'about': {
             id: 'about',
-            content: 'widgets/about.html'
+            uri: 'common-widgets/about.html'
         },
         'counter': {
             id: 'counter',
-            content: 'widgets/counter/'
+            uri: 'widgets/counter/'
         },
         'todos': {
             id: 'todos',
-            content: 'widgets/todos/'
+            uri: 'widgets/todos/'
         },
         'minance': {
             id: 'minance',
-            content: 'widgets/minance/'
+            uri: 'widgets/minance/'
         }
     }
 };
